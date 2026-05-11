d761c7f68a5d484f1d0f1fd96d6bdd32a21b39d0
lelouch-526 <iamgawd4545@gmail.com>
2026-05-11 11:00:28 +0530

    Minimal server test

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,6 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const projectRoutes = require("./routes/projectRoutes");
-const taskRoutes = require("./routes/taskRoutes");
-const authRoutes = require("./routes/authRoutes");
-const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
-
-app.use(cors({
-  origin: "*"
-}));
-app.use(express.json());
-
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("API Running");
@@ -23,15 +8,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 8080;
 
-app.use("/api/auth", authRoutes);
-app.use("/api/projects", projectRoutes);
-app.use("/api/tasks", taskRoutes);
-app.get("/", (req, res) => {
-res.send("API Running");
-});
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
-// app.use("/api/auth", authRoutes);
-// app.use("/api/projects", projectRoutes);
-// app.use("/api/tasks", taskRoutes);
